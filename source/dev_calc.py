4bd412150f531aa4a67c09018da81c71dcf79e96
Dtzz81 <denise.turkova@madetech.com>
2025-01-08 13:15:41 +0000

    standard deviation test pass, code done

--- source/dev_calc.py
+++ source/dev_calc.py
@@ -1,11 +1,32 @@
 import numpy as np
 
 def calculate(list):
+    pass
+
+def mean(list):
     reshaped_arr = np.array(list).reshape(3, 3)
+
     column_mean = np.mean(reshaped_arr, axis=0)
     row_mean = np.mean(reshaped_arr, axis=1)
     flattened_mean = np.mean(reshaped_arr)
     mean = [column_mean.tolist(), row_mean.tolist(), float(flattened_mean)]
+
     return mean
+
+def variance(list):
+    reshaped_arr = np.array(list).reshape(3, 3)
+    column_var = np.var(reshaped_arr, axis=0)
+    row_var = np.var(reshaped_arr, axis=1)
+    flattened_var = np.var(reshaped_arr)
+    variance = [column_var.tolist(), row_var.tolist(), float(flattened_var)]
+    return variance
+
+def std(list):
+    reshaped_arr = np.array(list).reshape(3, 3)
+    column_std = np.std(reshaped_arr, axis=0)
+    row_std = np.std(reshaped_arr, axis=1)
+    flattened_std = np.std(reshaped_arr)
+    standard_variation = [column_std.tolist(), row_std.tolist(), float(flattened_std)]
+    return standard_variation
 
 calculate([0,1,2,3,4,5,6,7,8])
--- tests/test_dev_calc.py
+++ tests/test_dev_calc.py
@@ -1,15 +1,32 @@
-from source.dev_calc import calculate
+from source.dev_calc import std, variance, mean
 
 def test_mean():
     # arrange
     list = [0,1,2,3,4,5,6,7,8]
     # act
-    result = calculate(list)
-    expected =[
+    result = mean(list)
+    expected_mean =[
             [3.0, 4.0, 5.0],  # convention to start with columns
             [1.0, 4.0, 7.0],
             4.0
         ]
      # assert
-    assert result == expected
+    assert result == expected_mean
 
+def test_variance():
+    # arrange
+    list = [0,1,2,3,4,5,6,7,8]
+    # act
+    result = variance(list)
+    expected_variance =[[6.0, 6.0, 6.0], [0.6666666666666666, 0.6666666666666666, 0.6666666666666666], 6.666666666666667]
+     # assert
+    assert result == expected_variance
+
+def test_standard_deviation():
+    # arrange
+    list = [0,1,2,3,4,5,6,7,8]
+    # act
+    result = std(list)
+    expected_std =[[2.449489742783178, 2.449489742783178, 2.449489742783178], [0.816496580927726, 0.816496580927726, 0.816496580927726], 2.581988897471611]
+     # assert
+    assert result == expected_std

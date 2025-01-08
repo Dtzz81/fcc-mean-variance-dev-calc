8353a227a65937c61a894235b9c99bdb7a24e4df
Dtzz81 <denise.turkova@madetech.com>
2025-01-08 10:53:37 +0000

    FCC assingment Mean-Variance-Standard Deviation Calculator

---
+++ tests/test_mean_var_dev_calc.py
@@ -0,0 +1,18 @@
+
+from source.mean_var_dev_calc import calculate
+
+
+def test_mean():
+    # arrange
+    list = [0,1,2,3,4,5,6,7,8]
+    # act
+    result = calculate(list)
+    expected = {
+        'mean': [
+            [3.0, 4.0, 5.0],  # convention to start with columns
+            [1.0, 4.0, 7.0],
+            4.0
+        ]
+    }
+     # assert
+    assert result == expected

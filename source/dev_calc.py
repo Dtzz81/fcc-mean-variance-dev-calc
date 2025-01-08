7fa7bfb7ffe2aae473de3f9809cae325ae2487d1
Dtzz81 <denise.turkova@madetech.com>
2025-01-08 14:00:55 +0000

    last calculation done

--- source/dev_calc.py
+++ source/dev_calc.py
@@ -1,8 +1,5 @@
 import numpy as np
 
-def calculate(list):
-    pass
-
 def mean(list):
     reshaped_arr = np.array(list).reshape(3, 3)
 
@@ -15,18 +12,63 @@ def mean(list):
 
 def variance(list):
     reshaped_arr = np.array(list).reshape(3, 3)
+
     column_var = np.var(reshaped_arr, axis=0)
     row_var = np.var(reshaped_arr, axis=1)
     flattened_var = np.var(reshaped_arr)
     variance = [column_var.tolist(), row_var.tolist(), float(flattened_var)]
+
     return variance
 
 def std(list):
     reshaped_arr = np.array(list).reshape(3, 3)
+
     column_std = np.std(reshaped_arr, axis=0)
     row_std = np.std(reshaped_arr, axis=1)
     flattened_std = np.std(reshaped_arr)
     standard_variation = [column_std.tolist(), row_std.tolist(), float(flattened_std)]
+
     return standard_variation
+
+def max(list):
+    reshaped_arr = np.array(list).reshape(3, 3)
+
+    column_max = np.max(reshaped_arr, axis=0)
+    row_max = np.max(reshaped_arr, axis=1)
+    flattened_max = np.max(reshaped_arr)
+    maximum = [column_max.tolist(), row_max.tolist(), float(flattened_max)]
+
+    return maximum
+
+def min(list):
+    reshaped_arr = np.array(list).reshape(3, 3)
+
+    column_min = np.min(reshaped_arr, axis=0)
+    row_min = np.min(reshaped_arr, axis=1)
+    flattened_min = np.min(reshaped_arr)
+    minimum = [column_min.tolist(), row_min.tolist(), float(flattened_min)]
+
+    return minimum
+
+def sum(list):
+    reshaped_arr = np.array(list).reshape(3, 3)
+
+    column_sum = np.sum(reshaped_arr, axis=0)
+    row_sum = np.sum(reshaped_arr, axis=1)
+    flattened_sum = np.sum(reshaped_arr)
+    total_sum = [column_sum.tolist(), row_sum.tolist(), float(flattened_sum)]
+
+    return total_sum
+
+def calculate(list):
+    calculations ={
+        'mean': mean(list),
+        'variance': variance(list),
+        'standard deviation': std(list),
+        'max': max(list),
+        'min': min(list),
+        'sum': sum(list)
+    }
+    return calculations
 
 calculate([0,1,2,3,4,5,6,7,8])
--- tests/test_dev_calc.py
+++ tests/test_dev_calc.py
@@ -1,4 +1,4 @@
-from source.dev_calc import std, variance, mean
+from source.dev_calc import std, variance, mean, max, min, sum, calculate
 
 def test_mean():
     # arrange
@@ -14,19 +14,49 @@ def test_mean():
     assert result == expected_mean
 
 def test_variance():
-    # arrange
     list = [0,1,2,3,4,5,6,7,8]
-    # act
     result = variance(list)
     expected_variance =[[6.0, 6.0, 6.0], [0.6666666666666666, 0.6666666666666666, 0.6666666666666666], 6.666666666666667]
-     # assert
+
     assert result == expected_variance
 
 def test_standard_deviation():
-    # arrange
     list = [0,1,2,3,4,5,6,7,8]
-    # act
     result = std(list)
     expected_std =[[2.449489742783178, 2.449489742783178, 2.449489742783178], [0.816496580927726, 0.816496580927726, 0.816496580927726], 2.581988897471611]
-     # assert
+
     assert result == expected_std
+
+def test_max():
+    list = [0,1,2,3,4,5,6,7,8]
+    result = max(list)
+    expected_max =[[6, 7, 8], [2, 5, 8], 8]
+
+    assert result == expected_max
+
+def test_min():
+    list = [0,1,2,3,4,5,6,7,8]
+    result = min(list)
+    expected_min = [[0, 1, 2], [0, 3, 6], 0]
+
+    assert result == expected_min
+
+def test_sum():
+    list = [0,1,2,3,4,5,6,7,8]
+    result = sum(list)
+    expected_sum = [[9, 12, 15], [3, 12, 21], 36]
+
+    assert result == expected_sum
+
+def test_calculate():
+    list = [0,1,2,3,4,5,6,7,8]
+    result = calculate(list)
+    expected_result = {
+  'mean': [[3.0, 4.0, 5.0], [1.0, 4.0, 7.0], 4.0],
+  'variance': [[6.0, 6.0, 6.0], [0.6666666666666666, 0.6666666666666666, 0.6666666666666666], 6.666666666666667],
+  'standard deviation': [[2.449489742783178, 2.449489742783178, 2.449489742783178], [0.816496580927726, 0.816496580927726, 0.816496580927726], 2.581988897471611],
+  'max': [[6, 7, 8], [2, 5, 8], 8],
+  'min': [[0, 1, 2], [0, 3, 6], 0],
+  'sum': [[9, 12, 15], [3, 12, 21], 36]
+}
+    assert result == expected_result
